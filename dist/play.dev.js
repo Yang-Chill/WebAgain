c183c3f546579e991cb4467c886f6eda16d31e7a
杨呈焕 <1716503853@qq.com>
2021-09-18 16:12:13 +0800

    ?

--- dist/play.dev.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-Promise.resolve(10).then(function (res) {
-  return res;
-});
--- dist/playground.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var p1 = Promise.resolve(10).then(function (res) {
-  return res;
-});
-console.log(p1);
-var p2 = Promise.resolve(p1);
-console.log(p2 === p1);
